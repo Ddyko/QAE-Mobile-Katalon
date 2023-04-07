6c6466c3dc9bd1143e862130976f6a685b2d291c
zihadubaddika <zihad.ubaddika@gmail.com>
2023-04-07 13:56:57 +0700

    fix gherkin syntax

--- Include/scripts/groovy/stepDefinition/bidProduct.groovy
+++ Include/scripts/groovy/stepDefinition/bidProduct.groovy
@@ -107,8 +107,8 @@ public class bidProduct {
 		Mobile.tap(findTestObject('Object Repository/Receive_Bid_Page/btn_terima'), 0)
 	}
 
-	@Then("Message appear that bid is accepted")
-	public void message_appear_that_bid_is_accepted() {
+	@Then("User can see message that bid is accepted")
+	public void user_can_see_message_that_bid_is_accepted() {
 		Mobile.verifyElementVisible(findTestObject('Object Repository/Receive_Bid_Page/btn_hubungi'), 0)
 	}
 
@@ -117,8 +117,8 @@ public class bidProduct {
 		Mobile.tap(findTestObject('Object Repository/Receive_Bid_Page/btn_tolak'), 0)
 	}
 
-	@Then("Message appear that product bid rejected")
-	public void message_appear_that_product_bid_rejected() {
+	@Then("User can see message that product bid rejected")
+	public void user_can_see_message_that_product_bid_rejected() {
 		Mobile.verifyElementVisible(findTestObject('Object Repository/Receive_Bid_Page/info_penawar'), 0)
 	}
 
